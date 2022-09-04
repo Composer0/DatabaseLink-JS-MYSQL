3e0188434ce43a75a5a06f22a836ca5a8ceae204
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-09-04 13:54:11 -0400

    Code for future use

---
+++ database.js
@@ -0,0 +1,13 @@
+const {createPool} = require('mysql2');
+
+const pool = createPool ({
+    host: "127.0.0.1",
+    port: "4274",
+    user: "Composer0",
+    password: "Desp0liation-S0ng",
+    connectionLimit: 10
+});
+
+pool.query(`select * from loginsystem.loginsystem`, (err, res)=>{
+    return console.log(res)
+});
